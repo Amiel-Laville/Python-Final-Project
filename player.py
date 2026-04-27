529911fad84f885d78a18defa61c0b06cb0a8a4c
Amiel-Laville <tuu69091@temple.edu>
2026-04-27 17:36:59 -0400

    Update player.py

--- player.py
+++ player.py
@@ -3,42 +3,34 @@ import random
 class Player:
     def __init__(self, color):
         self.color = color
-    
+
     def get_move(self, board):
         while True:
             try:
-                move_input = input(f"{self.color.capitalize()}'s turn. Enter move (row col row col): ")
-                parts = move_input.split()
-                if len(parts) != 4:
-                    print("Invalid format.")
-                    continue
+                move = input("Move (r c r c): ")
+                r1, c1, r2, c2 = map(int, move.split())
 
-                from_row, from_col, to_row, to_col = map(int, parts)
-
-                if board.move_piece((from_row, from_col), (to_row, to_col)):
+                if board.move_piece((r1,c1),(r2,c2)):
                     return
-                else:
-                    print("Invalid move. Try again.")
-
+                print("Invalid move")
             except:
-                print("Invalid input.")
-                
+                print("Bad input")
+
 
 class AIPlayer:
     def __init__(self, color):
         self.color = color
-    
-    def get_move(self, board):
-        print(f"AI ({self.color}) is making a move...")
 
+    def get_move(self, board):
         pieces = board.get_all_pieces(self.color)
 
-        random.shuffle(pieces)
+        moves = []
+        for p in pieces:
+            for m in p.get_valid_moves(board):
+                moves.append((p, m))
 
-        for (r, c) in pieces:
-            # try random moves
-            for dr in [-1, 0, 1]:
-                for dc in [-1, 0, 1]:
-                    new_r, new_c = r + dr, c + dc
-                    if board.move_piece((r, c), (new_r, new_c)):
-                        return
+        if not moves:
+            return
+
+        piece, move = random.choice(moves)
+        board.move_piece(piece.position, move)
